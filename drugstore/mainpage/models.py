28eff34ef2de0cbf50856616e5db0c68c9f54424
slopa <onetwosecond14488@gmail.com>
2018-12-21 01:37:44 +0200

    IT's finally huesos

--- drugstore/mainpage/models.py
+++ drugstore/mainpage/models.py
@@ -6,8 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField(default=0)
     location = models.CharField(max_length=70)
-    picture = models.ImageField
-    goods = models.Manager
+    goods = models.Manager()
 
     class Meta:
         db_table = 'Product'
